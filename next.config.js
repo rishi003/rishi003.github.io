53699b2d839e97339efcdcd3af61e1a8360b8a65
Rishabh Awatani <rishabh@nuevezo.com>
2023-05-13 12:10:41 -0400

    Configure Next for github pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === "production";
+/** @type {import('next').NextConfig} */
+const nextConfig = { output: "export" };
 
-module.exports = {
-  assetPrefix: isProd ? "/rishi003.github.io/" : "",
-  images: {
-    unoptimized: true,
-  },
-};
+module.exports = nextConfig;
